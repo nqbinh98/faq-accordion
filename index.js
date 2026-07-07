dd42743595b7fb31b83b98d5419c13c6371aad38
nqbinh <154815917+nqbinh98@users.noreply.github.com>
2026-07-07 13:39:18 +0700

    Upload html, css, js file

---
+++ index.js
@@ -0,0 +1,18 @@
+const containerContent = document.querySelector(".container-content");
+
+containerContent.addEventListener('click', function (e) {
+    if(e.target.closest('.question-section')) {
+        let answerWrapper = e.target.closest('.question-item').querySelector('.answer-section');
+        let btnPlus = e.target.closest('.question-section').querySelector('.btn-plus');
+        let btnMinus = e.target.closest('.question-section').querySelector('.btn-minus');
+        if (answerWrapper.classList.contains('show')) {
+            btnPlus.classList.remove('hide');
+            btnMinus.classList.add('hide');
+            answerWrapper.classList.remove('show');
+        } else {
+            btnPlus.classList.add('hide');
+            btnMinus.classList.remove('hide');
+            answerWrapper.classList.add('show');
+        }
+    }
+})
